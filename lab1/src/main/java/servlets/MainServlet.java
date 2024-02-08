a583c823f8e17ea2d243fb458a29a25059795d49
Danq1101 <danilsob2004@gmail.com>
2024-02-08 19:52:51 +0300

    Labs

---
+++ lab1/src/main/java/servlets/MainServlet.java
@@ -0,0 +1,13 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "text", urlPatterns = "/lab1/*")
+public class MainServlet extends HttpServlet {
+
+}
---
+++ lab1/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=utf-8" %>
+<html>
+<body>
+<h2>Сервлет</h2>
+<form action="test">
+    <input type="submit" value="Выполнить"> </form>
+</body>
+</html>
